7186c56966af9a10bfdf912fb7d1dc4a1cdae052
alirezaderakhshimtn-cmd <alirezaderakhshi.mtn@gmail.com>
2025-11-04 17:23:06 +0330

    Update sqli.go

--- sqli.go
+++ sqli.go
@@ -1,7 +1,1 @@
-// Vulnerable SQL Injection example (Go)
-func getUser(w http.ResponseWriter, r *http.Request) {
-    username := r.URL.Query().Get("username")
-    query := fmt.Sprintf("SELECT * FROM users WHERE username='%s'", username)
-    rows, _ := db.Query(query)
-    // ...
-}
+
